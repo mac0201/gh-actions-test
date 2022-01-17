0d8ae05dda37c16834bc27031149d023551a8790
Maciej Ornowski <mac0201@hotmail.co.uk>
2022-01-17 17:39:25 +0000

    fixed bugs

--- main.py
+++ main.py
@@ -2,15 +2,15 @@ import os
 import math
 
 
-def fun1(a, b, c) -> str:
+def fun1(a, b, c) -> int:
     return math.floor(a + b + c)
 
 
-def fun2(a, b) -> int:
+def fun2(a, b) -> str:
     return os.getcwd()
 
 
-def fun3(a) -> str:
+def fun3(a) -> int:
     return a / 4
 
 
